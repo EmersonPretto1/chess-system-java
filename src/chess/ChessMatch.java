5eeb221dde5fab3510859b6ed416a469a6a481f6
Emerson Pretto <emerson.pretto@gmail.com>
2022-02-02 02:51:21 +0044

    Moving Pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
 import boardgame.Position;
-import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -27,16 +27,46 @@ public class ChessMatch {
 		
 		
 	}
+	
+	public ChessPiece performChessmove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+				
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("There is not a piece on sourece position");
+		}
+		
+	}
+	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 	
-    private void initialSetup() {
-    	placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-    	placeNewPiece('e', 8, new King(board, Color.BLACK));
-    	placeNewPiece('e', 1, new King(board, Color.WHITE));
+    
+    	private void initialSetup() {
+            placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+            
+
+            placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+            
+            placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+            
+    	}
 		
     		
     	
-    }
+    
 }
